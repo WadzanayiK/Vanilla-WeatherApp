a792d55d7734dbec5e2eed18200ccd4edcf0bd07
WadzanayiK <67104735+WadzanayiK@users.noreply.github.com>
2022-10-19 10:56:52 +0200

    updated API

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function formatDate(timestamp) {
   
   function getForecast(coordinates) {
     console.log(coordinates);
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey = "25fad9f7e87157d33dde0f82ab269ee8";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
   }
@@ -89,7 +89,7 @@ function formatDate(timestamp) {
     getForecast(response.data.coord);
   }
   function search(city) {
-    let apiKey = "701f06352d61835bc4fc894e7b084629";
+    let apiKey = "25fad9f7e87157d33dde0f82ab269ee8";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
   }
